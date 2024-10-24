d6ee94b9f2d9965640a1c908b7955a5ab053fac8
Alexandre De Carvalho Pimentel <apimentel@firjan.com.br>
2024-10-24 15:39:55 -0300

    Recado completo

--- src/main/java/br/com/senai/recados1/controller/RecadoController.java
+++ src/main/java/br/com/senai/recados1/controller/RecadoController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +24,30 @@ public class RecadoController {
 	private RecadoService recadoService;
 	
 	@GetMapping
-	public ResponseEntity<List<Recado>> findAll(){
+	public ResponseEntity<List<Recado>> findAll(){ // Lista os recados
 		List<Recado> recados = recadoService.findAll();
 		return ResponseEntity.ok().body(recados);
 	}
 	@GetMapping("/{id}")
 	public ResponseEntity<Recado> findById(@PathVariable Long id){
 		Recado recado = recadoService.findById(id);
-		return ResponseEntity.ok().body(recado);
+		return ResponseEntity.ok().body(recado); // Mostra um recado pelo ID
 	}
-	
-	
+	@PostMapping
+	public ResponseEntity<Recado> insertNew(@RequestBody Recado recado){
+		Recado recadoInserido = recadoService.insertNew(recado);
+		return ResponseEntity.ok().body(recadoInserido); // Insere novo recado
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Recado> update(@PathVariable Long id, @RequestBody Recado recadoAlterado){
+		Recado recadoAtual = recadoService.update(id, recadoAlterado);
+		return ResponseEntity.ok().body(recadoAtual);
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteById(@PathVariable Long id){
+		Boolean flag = recadoService.deleteById(id);
+		return ResponseEntity.ok().body(flag);
+	}
 	
 	
 	
